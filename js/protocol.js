c02ecdb896bfdf0e8e0fa84ba86631327a17bd03
Amina Lazrak <amina.lazrak@gmail.com>
2018-07-11 17:31:13 -0400

    Update 2

---
+++ js/protocol.js
@@ -0,0 +1,7 @@
+//On clicking "start over", reset the entire scoresheet
+$(document).ready(function(){
+	$("#reset-all").on("click",function(){
+	/* Single line Reset function executes on click of Clear all Button */
+	$("#form")[0].reset();
+});});
+
