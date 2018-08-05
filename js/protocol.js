2d1ea06855df7f1811815058348d0f7fa8f4db2d
Amina Lazrak <amina.lazrak@gmail.com>
2018-08-05 16:52:51 -0400

    Update 3 v2

--- js/protocol.js
+++ js/protocol.js
@@ -1,7 +1,14 @@
-//On clicking "start over", reset the entire scoresheet
 $(document).ready(function(){
+	//on clicking "Clear All", reset the entire scoresheet
 	$("#reset-all").on("click",function(){
-	/* Single line Reset function executes on click of Clear all Button */
-	$("#form")[0].reset();
-});});
+		$("#form")[0].reset();
+	});
+	//on clicking clear in each section, reset only appropriate section
+	$("#reset").on("click",function(){
+		console.log('Hi');
+		$(this).parents('div').find('input:text').val('');
+		$(this).parents('div').find('input:date').val('');
+	});
+	//on clicking plus sign, add fields accordingly
 
+});
