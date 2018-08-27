9228b2bd460826bd72bbf55639da48112feb6ad1
Amina Lazrak <amina.lazrak@gmail.com>
2018-08-27 13:23:05 -0400

    Update 6 v2
    minus sign is added when click plus sign

--- js/protocol.js
+++ js/protocol.js
@@ -3,19 +3,27 @@ $(document).ready(function(){
 	$("#reset-all").on("click",function(){
 		$("#form")[0].reset();
 	});
+	
 	//on clicking clear in each section, reset only appropriate section
 	$("#reset").on("click",function(){
-		console.log($(this).closest('h2').html());
+		idClear=$(this).closest('.data-section').attr('id');
+		console.log('#'+idClear+'')
+
+		$('#'+idClear+'').find('input:text').val('');
+		$('#'+idClear+'').find('select').val(this.defaultSelected);
+		//$('#'+idClear+'').find('input:date').val('');
+		//$("#information")[0].reset();
 		// $(this).parents('div').find('input:text').val('');
 		// $(this).parents('div').find('input:date').val('');
 	});
-	//on clicking plus sign, add fields accordingly
+	
+	//on clicking plus sign, add fields accordingly and add minus sign
 	addItem=[];
 	$(".add").on("click",function(){
-		console.log(this.id);
 		addItem=$(this).closest('.fields-bin').find('.add-section').html();
-		console.log(addItem);
 		//$(this).closest('.fields-bin').append(addItem); this was to add the items to the parent div
-		$(this).append('<div style="float:left; margin-right:5px;"> '+addItem+'</div>');
+		$(this).closest('.fields-bin').append('<div style="float:right; margin-right:5px; display:block"> '+addItem+'</div>');
+		subtract=$(this).closest('.fields-bin').find('.remove');
+		$(subtract).css("display","inline-block");
 	});
 });
